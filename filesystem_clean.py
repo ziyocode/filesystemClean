24040f38bbf2673158fa5323d1ffa4852c316ea2
RyanCho <ziyocode@gmail.com>
2022-10-08 23:20:23 +0900

    GZIP method add

--- filesystem_clean.py
+++ filesystem_clean.py
@@ -11,18 +11,21 @@ import time
 import logging
 import re
 from datetime import datetime
+import gzip
+import shutil
 
 # setting Configuration File
-configFile = ".//filesystem_clean.conf"
+CONFIG_FILE = "./filesystem_clean.conf"
+
 
 # setting Logging configuration
-logdir = "/var/log/filesystem_clean"
-log_expired_date = 10  # integer : days
-logdate = datetime.today().strftime("%Y%m%d")
-log_filename = logdir + "/filesystem_clean_" + logdate + ".log"
+LOGDIR = "/tmp/filesystem_clean"
+LOG_EXPIRED_DATE = 10  # integer : days
+LOGDATE = datetime.today().strftime("%Y%m%d")
+LOG_FILENAME = LOGDIR + "/filesystem_clean_" + LOGDATE + ".log"
 
-if os.path.exists(logdir) == "False":
-    os.mkdir(logdir)
+if os.path.exists(LOGDIR) is False:
+    os.mkdir(LOGDIR)
 
 logger = logging.getLogger("filesystem_clean")
 logger.setLevel(logging.INFO)
@@ -32,12 +35,13 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 # create file handler and set level to debug
-fh = logging.FileHandler(log_filename)
+fh = logging.FileHandler(LOG_FILENAME)
 fh.setLevel(logging.INFO)
 
 # create formatter
 formatter = logging.Formatter(
-    fmt="%(asctime)s %(name)s [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S",
+    fmt="%(asctime)s %(name)s [%(levelname)s] - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 # add formatter to ch, fh
@@ -53,9 +57,11 @@ current_time = time.time()
 
 
 # start input-check
+
+
 def input_checker():
     try:
-        cf = open(configFile, "r")
+        cf = open(CONFIG_FILE, "r", encoding="UTF-8")
     except OSError as e:
         logger.info(e)
     else:
@@ -70,36 +76,36 @@ def input_checker():
                     mode = str(lineList[0])
                     work_dir = str(lineList[1])
                     scope = str(lineList[2])
-                    prefix = str(lineList[3])
+                    condition = str(lineList[3])
                     target_date = int(lineList[4])
                     # target_time = target_date * 24 * 60 * 60  # 1 day
                     method = str(lineList[5].rstrip())
+                    CONF_ERROR_MESSAGE = "-----> Configuration file error"
 
                     if mode == "RUN":
-                        logger.info(
-                            "Files will be deleted/initialized in real. RUNNING mode : actually delete!!"
-                        )
+                        logger.info("Files will be deleted/initialized in real. RUNNING mode : actually delete!!")
                     elif mode == "DEBUG":
                         logger.info("Files are not deleted/initialized. DEBUG mode : log only!!.")
                     else:
                         logger.error("Please check mode in configuration file.")
-                        sys.exit("-----> Configuration file error")
+                        sys.exit(CONF_ERROR_MESSAGE)
 
                     if os.path.isdir(work_dir):
                         logger.info("Direcotory is [%s]", work_dir)
                     else:
                         logger.error("Please check the Directory in configuration file.")
-                        sys.exit("-----> Configuration file error")
+                        sys.exit(CONF_ERROR_MESSAGE)
 
                     if scope == "ALL":
                         logger.info("All files are subject to delete/initialize.")
                     elif scope == "SPECIFIC":
                         logger.info(
-                            "Files that start with [%s] are subject to delete/initialize.", prefix,
+                            "Files that start with [%s] are subject to delete/initialize.",
+                            condition,
                         )
                     else:
                         logger.error("Please check the scope in configuration file.")
-                        sys.exit("-----> Configuration file error")
+                        sys.exit(CONF_ERROR_MESSAGE)
 
                     logger.info("Target Date is [%i]", target_date)
 
@@ -107,61 +113,70 @@ def input_checker():
                         logger.info("method is [%s]", method)
                     elif method == "NULL":
                         logger.info("method is [%s]", method)
+                    elif method == "GZIP":
+                        logger.info("method is [%s]", method)
                     else:
                         logger.error("Please check the method in configuration file.")
-                        sys.exit("-----> Configuration file error")
+                        sys.exit(CONF_ERROR_MESSAGE)
                 else:
                     logger.error("Please check the configuration file - num(entity) is wrong.!!")
-                    sys.exit("-----> Configuration file error")
+                    sys.exit(CONF_ERROR_MESSAGE)
         logger.info("-" * 70)
         cf.close()
 
 
-def _automethod(running_mode, method_type, target_file):
+def automethod(running_mode, method_type, target_file):
     if method_type == "DELETE" and "filesystem_clean" not in target_file:
         if running_mode == "RUN":
             logging.info("[RUNNING] Deleted File : %s", target_file)
             os.remove(target_file)
         else:
             logger.info("[DEBUGE] Deleted file : %s", target_file)
-    elif (
-        method_type == "NULL"
-        and os.path.getsize(target_file) != 0
-        and "filesystem_clean" not in target_file
-    ):
+    elif method_type == "NULL" and os.path.getsize(target_file) != 0 and "filesystem_clean" not in target_file:
         if running_mode == "RUN":
             logger.info("[RUNNING] Initialized File : %s", target_file)
-            nf = open(target_file, "w")
+            nf = open(target_file, "w", encoding="UTF-8")
             nf.close()
         else:
             logger.info("[DEBUG] Initialized File : %s", target_file)
+    elif method_type == "GZIP" and os.path.getsize(target_file) != 0:
+        if running_mode == "RUN":
+            logger.info("[RUNNING] gzip File : %s", target_file)
+            with open(target_file, "rb") as f_in:
+                with gzip.open(target_file, "wb") as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+                    os.rename(target_file, target_file + ".gz")
+        else:
+            logger.info("[DEBUG] gzip File : %s", target_file)
 
 
-def autodelete(mode, work_dir, scope, prefix, target_time, method):
+def autodelete(mode, work_dir, scope, condition, target_time, method):
     for (root, path, files) in os.walk(work_dir):
         for filename in files:
             file_mtime = os.stat(os.path.join(root, filename)).st_mtime
             file_fullpath = os.path.join(root, filename)
             if (current_time - file_mtime) > target_time:
                 if scope == "ALL":
-                    _automethod(mode, method, file_fullpath)
+                    automethod(mode, method, file_fullpath)
                 elif scope == "SPECIFIC":
-                    if filename.startswith(prefix):
-                        _automethod(mode, method, file_fullpath)
+                    condition_list = condition.split(",")
+                    if len(condition_list) > 1:
+                        if filename.startswith(condition_list[0]) and condition_list[1] in filename:
+                            automethod(mode, method, file_fullpath)
                     else:
-                        continue
+                        if filename.startswith(prefix):
+                            automethod(mode, method, file_fullpath)
                 else:
                     logger.error("Please check the scope in configuration file.")
                     break
 
 
 def find_empty_dirs(running_mode, work_dir, scope, method):
-    monthFormat = re.compile("/([2]\d{3}(0[1-9]|1[0-2]))|([2]\d{3}/(0[1-9]|1[0-2]))")
     if scope == "ALL" and method == "DELETE":
         logger.info("start deleting empty directories.")
         logger.info("-" * 70)
         for (path, dir, files) in os.walk(work_dir):
-            if len(dir) == 0 and len(files) == 0 and monthFormat.search(path) != None:
+            if len(dir) == 0 and len(files) == 0 and (re.compile("/([2]\d{3}(0[1-9]|1[0-2]))|([2]\d{3}/(0[1-9]|1[0-2]))")).search(path) != None:
                 if running_mode == "RUN":
                     logger.info("[RUNNING] Deleted directory: %s", path)
                     os.rmdir(path)
@@ -179,51 +194,53 @@ def own_log_delete(logdir, log_expired_date):
                 os.remove(file_fullpath)
 
 
-# configuration file check
-logger.info("-" * 70)
-logger.info("filesystem_clean.py start!!")
-logger.info("-" * 70)
-logger.info("-" * 70)
-logger.info("Configuration file : filesystem_clean.conf file check")
-logger.info("-" * 70)
-input_checker()
-logger.info("Configuration file check is successfully completed.")
-logger.info("-" * 70)
+if __name__ == "__main__":
 
-# main fuction start
-with open(configFile, "r") as cf:
-    for line in cf:
-        if line.startswith("#") or line.lstrip() == "":
-            continue
-        else:
-            lineList = line.split(":")
-            mode = str(lineList[0])
-            work_dir = str(lineList[1])
-            scope = str(lineList[2])
-            prefix = str(lineList[3])
-            target_date = int(lineList[4])
-            target_time = target_date * 24 * 60 * 60
-            method = str(lineList[5].rstrip())
-            logger.info(
-                "[MODE:%s] [DIR:%s] [SCOPE:%s] [PREFIX:%s] [TARGET TIME:%i days] [METHOD:%s]",
-                mode,
-                work_dir,
-                scope,
-                prefix,
-                target_date,
-                method,
-            )
-            logger.info("-" * 70)
-            autodelete(mode, work_dir, scope, prefix, target_time, method)
-            logger.info("-" * 70)
-            find_empty_dirs(mode, work_dir, scope, method)
-            logger.info("-" * 70)
+    # configuration file check
+    logger.info("-" * 70)
+    logger.info("filesystem_clean.py start!!")
+    logger.info("-" * 70)
+    logger.info("-" * 70)
+    logger.info("Configuration file : filesystem_clean.conf file check")
+    logger.info("-" * 70)
+    input_checker()
+    logger.info("Configuration file check is successfully completed.")
+    logger.info("-" * 70)
 
-logger.info("start deleting %s logs", logdir)
-logger.info("-" * 70)
+    # main fuction start
+    with open(CONFIG_FILE, "r", encoding="UTF-8") as conf:
+        for condition in conf:
+            if condition.startswith("#") or condition.lstrip() == "":
+                continue
+            else:
+                conditionList = condition.split(":")
+                MODE = str(conditionList[0])
+                WORK_DIR = str(conditionList[1])
+                SCOPE = str(conditionList[2])
+                CONDITION = str(conditionList[3])
+                TARGET_DATE = int(conditionList[4])
+                TARGET_TIME = TARGET_DATE * 24 * 60 * 60
+                METHOD = str(conditionList[5].rstrip())
+                logger.info(
+                    "[MODE:%s] [DIR:%s] [SCOPE:%s] [CONDITION:%s] [TARGET TIME:%i days] [METHOD:%s]",
+                    MODE,
+                    WORK_DIR,
+                    SCOPE,
+                    CONDITION,
+                    TARGET_DATE,
+                    METHOD,
+                )
+                logger.info("-" * 70)
+                autodelete(MODE, WORK_DIR, SCOPE, CONDITION, TARGET_TIME, METHOD)
+                logger.info("-" * 70)
+                find_empty_dirs(MODE, WORK_DIR, SCOPE, METHOD)
+                logger.info("-" * 70)
 
-own_log_delete(logdir, log_expired_date)
+    logger.info("start deleting %s logs", LOGDIR)
+    logger.info("-" * 70)
 
-logger.info("-" * 70)
-logger.info("filesystem_clean.py is successfully completed.")
-logger.info("-" * 70)
+    own_log_delete(LOGDIR, LOG_EXPIRED_DATE)
+
+    logger.info("-" * 70)
+    logger.info("filesystem_clean.py is successfully completed.")
+    logger.info("-" * 70)
